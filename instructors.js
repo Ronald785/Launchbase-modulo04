6a873a8b954954fd3e0b32881fedb6ed1abec511
Ronald <ronaldmateus785@gmail.com>
2021-04-30 22:08:25 -0300

    manipulating data

--- instructors.js
+++ instructors.js
@@ -1,10 +1,38 @@
 const fs = require('fs')
 //File System do node 
 const data = require('./data.json')
+const { age } = require('./util')
+
+//SHOW
+exports.show = function (req, res){
+    //1º req.query.id na url: /?id=1 || 2º req.body direto do formulário pelo POST
+    //3º req.params.id
+    const { id } = req.params
+    
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if (!foundInstructor){
+        return res.send("Instructor not found!")
+    }
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: "",
+    }
+
+
+    return res.render("instructors/show", { instructor })
+}
+
 
 //CREATE
 exports.post = function(req, res){
     //Validando se todos os dados estão preenchidos
+    //Object é um constructor 
     const keys = Object.keys(req.body)
 
     for( key of keys){
@@ -13,12 +41,25 @@ exports.post = function(req, res){
             return res.send("Please, fill all fields!")
         }
     }
+
+    let { avatar_url, birth, name, services, gender } = req.body
+
     //Convertendo data em milissegundos
-    req.body.birth = Date.parse(req.body.birth) 
-    req.body.created_at = Date.now()
+    birth = Date.parse(req.body.birth) 
+    const created_at = Date.now()
+    //Criando um id 
+    const id = Number(data.instructors.length + 1)
 
-    data.instructors.push(req.body)
-
+    data.instructors.push({
+        id,
+        avatar_url, 
+        name,
+        birth, 
+        gender,
+        services, 
+        created_at
+    })
+                              //Transformando objeto normal em obj JSON
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if (err) {
             return res.send("Write file error!")
